30f381d332a2372d553eca889ae88ec5a2c96e70
daboyspace <daboyspace@gmail.com>
2023-02-07 21:34:35 +0300

    CreateTask27

---
+++ Task27.py
@@ -0,0 +1,19 @@
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов или символами конца строки.Определите,
+# сколько различных слов содержится в этом тексте.
+# Input: She sells sea shells on the sea shore;The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore,I'm sure that the shells are sea
+# shore shells.
+# Output: 13
+
+# string = input()
+string = "She sells sea shells on the sea shore;The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore,I'm sure that the shells are sea shore shells."
+
+string = string.replace('.',' ').replace(',',' ').replace(';',' ')
+set1 = (string.upper().split())
+print(set1)
+print(len(set1))
